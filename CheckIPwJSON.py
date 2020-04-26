f8689984f4095d5f53160d90b2e1704fff65cb43
Bryce Breedlove <brycebreedlo@gmail.com>
2020-04-26 09:42:42 -0500

    first working version

--- CheckIPwJSON.py
+++ CheckIPwJSON.py
@@ -1,8 +1,28 @@
+# Native imports
 import json
 import ipaddress
+# Import local file -- (moved JSON data to its own file)
+from device_data import deviceJSON
 
-deviceJSON = '{"Version": "15.6", "locationN": "500 Northridge", "role": "Access", "upTime": "12:10:53.49", "hostname": "ATL-3650-1", "macAddress": "39:58:1f:9e:38:c1", "series": "Cisco Catalyst 3650 Series Switches", "lastUpdated": "2017-09-21 13:12:46", "bootDateTime": "2016-10-27 05:24:53", "interfaceCount": "24", "lineCardCount": "1", "managementIpAddress": "192.168.10.10", "interfaces": {"interface": [{"GigabitEthernet0": {"ipv4": "100.100.100.1"}}, {"GigabitEthernet1": {"ipv4": "10.10.10.2"}}]}}'
 
 #print(f'The Python data format for the "deviceJSON" variable is {type(deviceJSON)}')
 #print()
 #print(deviceJSON)
+
+def is_private_address(address: str) -> bool:
+    return ipaddress.IPv4Address(address).is_private
+
+
+if __name__ == '__main__':
+    # DeviceJSON is a 'str' so json.loads() is used to handle str input.
+    # json.load() takes a file object or a, "'.read()'--supporting file-like object containing a JSON document."
+    device_data = json.loads(deviceJSON)
+
+    for interfaces in device_data['interfaces']['interface']:
+
+        for interface, address in interfaces.items():
+
+            if is_private_address(address['ipv4']):
+                print(f'{interface} has an IP address of {address["ipv4"]} and is a Private Address')
+            else:
+                print(f'{interface} has an IP address of {address["ipv4"]} and is not a Private Address')
